fc813a20f8b7ba0a653cc74f5f24e1e1a7402cfb
mutebinuhu <mutebinuhu1@gmail.com>
2025-05-09 18:47:37 +0400

    chore:initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react'
+const App = () => {
+
+  return (
+    <h1 class="text-3xl font-bold underline text-red-500">
+    Hello world!
+  </h1>
+
+  )
+}
+
+export default App;
